4c78c2f69de32e6148b98c220ca2ed11f81312b6
Alejandro <alexchaconortega@gmail.com>
2025-05-13 16:49:12 +0200

    Update DatabaseConfig.java

--- Week_2/Day_1/src/config/DatabaseConfig.java
+++ Week_2/Day_1/src/config/DatabaseConfig.java
@@ -3,6 +3,6 @@ package Week_2.Day_1.src.config;
 public class DatabaseConfig {
     public static final String URL = "jdbc:mysql://localhost:3306/empresa_origen";
     public static final String USER = "root";
-    public static final String PASSWORD = "Alejandro2004#";
+    public static final String PASSWORD = "password";
     public static final String DRIVER = "com.mysql.cj.jdbc.Driver";
 }
