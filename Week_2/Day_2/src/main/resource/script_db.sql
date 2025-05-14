a6d4a26ce474a5778749768b21cbb62fbe9aa531
Alejandro <alexchaconortega@gmail.com>
2025-05-14 17:18:52 +0200

    Implement Inserts in script_db.sql

--- Week_2/Day_2/src/main/resource/script_db.sql
+++ Week_2/Day_2/src/main/resource/script_db.sql
@@ -13,3 +13,15 @@ CREATE TABLE resultados_estudiantes (
                                         promedio DOUBLE NOT NULL,
                                         desviacion DOUBLE NOT NULL
 );
+INSERT INTO usuarios (id, nombre, email) VALUES
+                                             (1, 'Ana Martínez', 'ana.martinez@email.com'),
+                                             (2, 'Luis Pérez', 'luis.perez@email.com'),
+                                             (3, 'María López', 'maria.lopez@email.com'),
+                                             (4, 'Carlos Gómez', 'carlos.gomez@email.com'),
+                                             (5, 'Lucía Fernández', 'lucia.fernandez@email.com'),
+                                             (6, 'Pedro Sánchez', 'pedro.sanchez@email.com'),
+                                             (7, 'Elena Ruiz', 'elena.ruiz@email.com'),
+                                             (8, 'David Romero', 'david.romero@email.com'),
+                                             (9, 'Sofía Castro', 'sofia.castro@email.com'),
+                                             (10, 'Javier Ortega', 'javier.ortega@email.com')
+;
